41e19d6d714e59e59ffa93de37dfc8312e0a4ef9
jagan <jagan5043@gmail.com>
2021-09-20 01:38:22 +0530

    sajnsjakdnlkasdk

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 import Dashboard from './components/Dashboard'
 import Header from './components/Layout/Header'
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router,Route} from "react-router-dom";
-import AddProject from './project/AddProject'
+import AddProject from './components/project/AddProject';
 
 class App extends Component{
   render(){
@@ -12,7 +13,8 @@ class App extends Component{
       <Router>
       <div className="App">
       <Header/>
-      <Dashboard/>
+      
+      <Route exact path="/dashboard" component={Dashboard}/>
       <Route exact path="/addProject" component={AddProject}/>
       </div>
       </Router>
