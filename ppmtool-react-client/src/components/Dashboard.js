dae9709311b39ea290d5c0af87f33337b5757a83
jagan <jagan5043@gmail.com>
2021-09-20 01:27:26 +0530

    ajsdbsjkksalkqjs

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,14 +1,27 @@
 import React, { Component } from 'react'
 import ProjectItem from './project/ProjectItem'
+import CreateProjectButton from './project/CreateProjectButton'
 
 
 class Dashboard extends Component {
     render() {
         return (
-            <div className="alert alert-warning">
-            <h1>Welcome to Dashboard</h1>
-            <ProjectItem/>
-            </div>)
+            <div className="projects">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-12">
+                        <h1 className="display-4 text-center">Projects</h1>
+                        <br />
+                        <CreateProjectButton/>
+                        <br />
+                        <hr />
+                        <ProjectItem/>
+    
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
     }
 }
 
